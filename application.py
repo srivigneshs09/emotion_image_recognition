9bd6f781970c6950250cbbe9d053850c9728080f
Sasi Kumar T <tmsasikumar@gmail.com>
2024-09-01 00:22:48 +0530

    Change port to 80

--- application.py
+++ application.py
@@ -41,4 +41,4 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0',port=8080, debug=True)
+    application.run(host='0.0.0.0',port=80, debug=True)
